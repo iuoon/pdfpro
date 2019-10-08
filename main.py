7f2cf6000e11e88910950919decc055a887cdf60
iuoon <mhwuyz@163.com>
2019-10-08 17:53:58 +0800

    修改代码

--- main.py
+++ main.py
@@ -8,4 +8,13 @@ if __name__ == '__main__':
     with open("test.html", 'r', encoding='utf-8') as f:
         content = f.read()
     content = content.replace("{替换标签}", str("替换结果"))
-    pdfkit.from_string(content, "test.pdf", configuration=config)
+    options = {
+        'encoding': "utf-8",
+        'page-height': '80mm',  # 设置宽
+        'page-width': '100mm',  # 设置高
+        'margin-top': '0mm',
+        'margin-right': '0mm',
+        'margin-bottom': '0mm',
+        'margin-left': '0mm'
+    }
+    pdfkit.from_string(content, "test.pdf", options=options, configuration=config)
