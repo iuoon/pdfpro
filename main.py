c693c4e6e7bb65938645bb425a0e3f80e5d9e159
iuoon <mhwuyz@163.com>
2019-10-12 17:51:28 +0800

    修改代码

--- main.py
+++ main.py
@@ -6,81 +6,103 @@ import win32api,win32print
 from PIL import ImageFont
 from PyPDF2 import PdfFileReader, PdfFileWriter, PdfFileMerger
 from jinja2 import Environment, FileSystemLoader
+import wx
 
 
-# 生成html模板
-def generate_html(body):
-    env = Environment(loader=FileSystemLoader('./'))
-    template = env.get_template('temp.html')
-    with open("template.html", 'w+', encoding='utf-8') as fout:
-        html_content = template.render(body=body)
-        fout.write(html_content)
+APP_TITLE = u'标签生成器'
+APP_ICON = 'res/python.ico'
 
+class mainFrame(wx.Frame):
+    '''程序主窗口类，继承自wx.Frame'''
+    def __init__(self, parent):
+        '''构造函数'''
+        wx.Frame.__init__(self, parent, -1, APP_TITLE)
+        self.SetBackgroundColour(wx.Colour(255, 255, 255))
+        self.SetSize((600, 480))
+        self.Center()
 
-# 生成pdf
-def generate_pdf(content):
-    # 生成pdf页，并设置大小
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    path_wk = BASE_DIR + '\\w\\wkhtmltopdf.exe'
-    config = pdfkit.configuration(wkhtmltopdf=path_wk)
-    options = {
-        'encoding': "utf-8",
-        'page-height': '50mm',  # 设置高
-        'page-width': '80mm',  # 设置宽
-        'margin-top': '0mm',
-        'margin-right': '0mm',
-        'margin-bottom': '0mm',
-        'margin-left': '0mm'
-    }
-    pdfkit.from_string(content, "temp.pdf", options=options, configuration=config)
+        # 生成html模板
+    def generate_html(self,body):
+        env = Environment(loader=FileSystemLoader('./'))
+        template = env.get_template('temp.html')
+        with open("template.html", 'w+', encoding='utf-8') as fout:
+            html_content = template.render(body=body)
+            fout.write(html_content)
+    
+    # 生成pdf
+    def generate_pdf(self,content):
+        # 生成pdf页，并设置大小
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        path_wk = BASE_DIR + '\\w\\wkhtmltopdf.exe'
+        config = pdfkit.configuration(wkhtmltopdf=path_wk)
+        options = {
+            'encoding': "utf-8",
+            'page-height': '50mm',  # 设置高
+            'page-width': '80mm',  # 设置宽
+            'margin-top': '0mm',
+            'margin-right': '0mm',
+            'margin-bottom': '0mm',
+            'margin-left': '0mm'
+        }
+        pdfkit.from_string(content, "temp.pdf", options=options, configuration=config)
+    
+        #创建一个pdf文件合并对象
+        # pdfMerger = PdfFileMerger()
+        # if (os.path.exists(BASE_DIR + "\\result.pdf")):
+        #     with open("result.pdf", 'rb') as fe1:
+        #         pdfMerger.append(fe1)
+        # with open("temp.pdf", 'rb') as fe2:
+        #     pdfMerger.append(fe2)
+        # with open("result.pdf", 'ab+') as fe3:
+        #     pdfMerger.write(fe3)
+    
+        pdfFileReader = PdfFileReader(open("temp.pdf", 'rb'))
+        pdfFileWriter = PdfFileWriter()
+    
+        if (os.path.exists(BASE_DIR + "\\result.pdf")):
+            pdfFileReader0 = PdfFileReader(open("result.pdf", 'rb'))
+            for page in range(pdfFileReader0.getNumPages()):
+                # 将每页添加到writer对象
+                pdfFileWriter.addPage(pdfFileReader0.getPage(page))
+        pageObj = pdfFileReader.getPage(0)
+        pdfFileWriter.addPage(pageObj)
+        pdfFileWriter.write(open("result.pdf", 'ab+'))
+    
+    
+    def start(self):
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        if (os.path.exists(BASE_DIR + "\\result.pdf")):
+            os.remove(BASE_DIR + "\\result.pdf")
+    
+        workbook = openpyxl.load_workbook("data.xlsx")
+        sheet = workbook.get_active_sheet()
+        title_data = []
+        for i in range(1, sheet.max_column + 1):
+            title_data.append(sheet.cell(1, i).value)
+        self.generate_html(title_data)
+    
+        for i in range(2, sheet.max_row + 1):
+            content = ''
+            with open("template.html", 'r', encoding='utf-8') as f:
+                content = f.read()
+            for j in range(1, sheet.max_column + 1):
+                content = content.replace("#"+str(j)+"#", str(sheet.cell(i, j).value))
+            self.generate_pdf(content)
+    
+        # font = ImageFont.truetype(font='simsun.ttc', size=20)
+        # text = "hello"
+        # width, height = font.getsize(text)
+        # print(width, height)
+        printers = win32print.GetDefaultPrinter()
+        print(printers)
 
-    #创建一个pdf文件合并对象
-    # pdfMerger = PdfFileMerger()
-    # if (os.path.exists(BASE_DIR + "\\result.pdf")):
-    #     with open("result.pdf", 'rb') as fe1:
-    #         pdfMerger.append(fe1)
-    # with open("temp.pdf", 'rb') as fe2:
-    #     pdfMerger.append(fe2)
-    # with open("result.pdf", 'ab+') as fe3:
-    #     pdfMerger.write(fe3)
+class mainApp(wx.App):
+    def OnInit(self):
+        self.SetAppName(APP_TITLE)
+        self.Frame = mainFrame(None)
+        self.Frame.Show()
+        return True
 
-    pdfFileReader = PdfFileReader(open("temp.pdf", 'rb'))
-    pdfFileWriter = PdfFileWriter()
-
-    if (os.path.exists(BASE_DIR + "\\result.pdf")):
-        pdfFileReader0 = PdfFileReader(open("result.pdf", 'rb'))
-        for page in range(pdfFileReader0.getNumPages()):
-            # 将每页添加到writer对象
-            pdfFileWriter.addPage(pdfFileReader0.getPage(page))
-    pageObj = pdfFileReader.getPage(0)
-    pdfFileWriter.addPage(pageObj)
-    pdfFileWriter.write(open("result.pdf", 'ab+'))
-
-
-if __name__ == '__main__':
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    if (os.path.exists(BASE_DIR + "\\result.pdf")):
-        os.remove(BASE_DIR + "\\result.pdf")
-
-    workbook = openpyxl.load_workbook("data.xlsx")
-    sheet = workbook.get_active_sheet()
-    title_data = []
-    for i in range(1, sheet.max_column + 1):
-        title_data.append(sheet.cell(1, i).value)
-    generate_html(title_data)
-
-    for i in range(2, sheet.max_row + 1):
-        content = ''
-        with open("template.html", 'r', encoding='utf-8') as f:
-            content = f.read()
-        for j in range(1, sheet.max_column + 1):
-            content = content.replace("#"+str(j)+"#", str(sheet.cell(i, j).value))
-        generate_pdf(content)
-
-    # font = ImageFont.truetype(font='simsun.ttc', size=20)
-    # text = "hello"
-    # width, height = font.getsize(text)
-    # print(width, height)
-    printers = win32print.GetDefaultPrinter()
-    print(printers)
-
+if __name__ == "__main__":
+    app = mainApp()
+    app.MainLoop()
